10f6d6b3b979575890ddc675778262fff654161d
byio <ben@benyang.io>
2018-05-15 19:22:32 +0800

    create server using node's http module, listening on process.env.PORT or port 6000 (localhost)

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const app = require('./app');
+
+const port = process.env.PORT || 6000;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Graphql server is listening for requests on ${port}.`);
+});

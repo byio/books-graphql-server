71cf4a3ec0b236f5cc0871f7254e4746aa9bd67f
byio <ben@benyang.io>
2018-06-01 02:21:24 +0800

    add books as a field in RootQuery, which returns a GraphQLList of BookType

--- schema/schema.js
+++ schema/schema.js
@@ -124,6 +124,12 @@ const RootQuery = new GraphQLObjectType({
         // code to get data from db or other sources
         return _.find(authors, { id: args.id });
       }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
     }
   }
 });

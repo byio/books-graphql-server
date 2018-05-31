224ddcf12c0b95091edfdfcbd6d37b040d79b286
byio <ben@benyang.io>
2018-06-01 02:10:59 +0800

    add new GraphQLList of BookType in AuthorType, and returned a filtered array based on authorId in book in the resolve function

--- schema/schema.js
+++ schema/schema.js
@@ -6,7 +6,8 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql;
 
 // dummy data
@@ -89,7 +90,14 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        // code to get data using property on parent object
+        return _.filter(books, { authorId: parent.id })
+      }
+    }
   })
 });
 

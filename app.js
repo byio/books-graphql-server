92b65cf18560d285876077b2570e77b25b3d2cc7
byio <ben@benyang.io>
2018-06-01 15:32:07 +0800

    set up dotenv to store mlab username and password, and connect to mlab from app.js using mongoose ORM

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 
 const schema = require('./schema/schema');
 
 const app = express();
+
+// import and configure dotenv
+require('dotenv').config();
+
+// connect to mlab
+const mLabURI = `mongodb://${process.env.MLAB_USERNAME}:${process.env.MLAB_PASSWORD}@ds139964.mlab.com:39964/books-graphql`;
+mongoose.connect(mLabURI)
+        .then(console.log('Connected to mLab.'));
 
 // set up graphql middleware
 app.use('/graphql', graphqlHTTP({

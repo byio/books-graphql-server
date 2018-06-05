808e88adb3cf548a5b7b213b87d79e719631191a
byio <ben@benyang.io>
2018-06-05 18:32:52 +0800

    install cors npm package, and set up middleware that allows cors on all routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const schema = require('./schema/schema');
 
@@ -8,6 +9,9 @@ const app = express();
 
 // import and configure dotenv
 require('dotenv').config();
+
+// middleware: allow cors for all routes
+app.use(cors());
 
 // connect to mlab
 const mLabURI = `mongodb://${process.env.MLAB_USERNAME}:${process.env.MLAB_PASSWORD}@ds139964.mlab.com:39964/books-graphql`;
